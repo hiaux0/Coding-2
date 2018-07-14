e068693da1ef6d3c1b4eeacbbf409591036d8c8f
hdn <hieudn5293@yahoo.de>
2018-07-14 11:15:26 +0200

    * Initial commit for the lambda calculus branch * Includes definition of l_true, l_false, l_not, l_and

--- MySql/replit.js
+++ MySql/replit.js
@@ -1,3 +1,29 @@
-let a = ['a', 'b', 'c'] /*?*/
+// const λ_true = x => y => x
+// λ_true(1)(2) /*?*/
 
-a.join("','") /*?*/
+// const λ_false = x => y => y
+// λ_false(1)(2) /*?*/
+
+// const λ_not = (bool, a, b) => {
+//   bool(a)(b)
+//   console.log(bool(a)(b))
+// }
+
+// λ_not(λ_true, λ_false, λ_true) /*?*/
+// λ_not(λ_false, λ_false, λ_true) /*?*/
+
+
+// This is already close to the formal definition.
+// Is there still a better way?
+
+(x => y => x)(1)(2) /*?*/
+const what = s => s(s) // applies the function to itself
+const id = x => x
+what(id) /*?*/
+// id => id(id)
+// (x => x) => (x => x)(x => x)
+// returns (x => x)
+
+f => x => f(x)
+
+
