dc80fcf5e389e2603dd98eb92b63e47f0277a8fb
hdn <hieudn5293@yahoo.de>
2018-07-29 12:07:29 +0200

    typicode's json online server #13

---
+++ db.js
@@ -0,0 +1,106 @@
+[ 
+  {
+    "command": "Command-a :",
+    "position": 1
+  },
+  {
+    "command": "Command-b :", 
+    "position": 2
+  },
+  {
+    "command": "Command-c :", 
+    "position": 3
+  },
+  {
+    "command": "Command-d :", 
+    "position": 4
+  },
+  {
+    "command": "Command-e :", 
+    "position": 5
+  },
+  {
+    "command": "Command-f :", 
+    "position": 6
+  },
+  {
+    "command": "Command-g :", 
+    "position": 7
+  },
+  {
+    "command": "Command-h :", 
+    "position": 8
+  },
+  {
+    "command": "Command-i :", 
+    "position": 9
+  },
+  {
+    "command": "Command-j :", 
+    "position": 10
+  },
+  {
+    "command": "Command-k :", 
+    "position": 11
+  },
+  {
+    "command": "Command-l :", 
+    "position": 12
+  },
+  {
+    "command": "Command-m :", 
+    "position": 13
+  },
+  {
+    "command": "Command-n :", 
+    "position": 14
+  },
+  {
+    "command": "Command-o :", 
+    "position": 15
+  },
+  {
+    "command": "Command-p :", 
+    "position": 16
+  },
+  {
+    "command": "Command-q :", 
+    "position": 17
+  },
+  {
+    "command": "Command-r :", 
+    "position": 18
+  },
+  {
+    "command": "Command-s :", 
+    "position": 19
+  },
+  {
+    "command": "Command-t :", 
+    "position": 20
+  },
+  {
+    "command": "Command-u :", 
+    "position": 21
+  },
+  {
+    "command": "Command-v :", 
+    "position": 22
+  },
+  {
+    "command": "Command-w :", 
+    "position": 23
+  },
+  {
+    "command": "Command-x :", 
+    "position": 24
+  },
+  {
+    "command": "Command-y :", 
+    "position": 25
+  },
+  {
+    "command": "Command-z :",
+    "position": 26
+  }  
+]
