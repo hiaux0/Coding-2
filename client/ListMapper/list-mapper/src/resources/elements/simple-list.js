b6b1bd1be8c098882d53ed34ca4de85da94489be
hdn <hieudn5293@yahoo.de>
2018-08-09 23:00:06 +0200

    Sort via Icons * [BUG] a17g13o8o

--- client/ListMapper/list-mapper/src/resources/elements/simple-list.js
+++ client/ListMapper/list-mapper/src/resources/elements/simple-list.js
@@ -1,52 +1,21 @@
-import {bindable, inject} from 'aurelia-framework';
+import {bindable} from 'aurelia-framework';
 import './simple-list.less';
-import db from '../../../../../../db';
-import { EventAggregator } from 'aurelia-event-aggregator';
 
-/**
- * Takes in 2 objects with an `position` property, and sorts them according to that `position`.
- * 
- * @param {Object} a - object with order property
- * @param {Object} b - object with order property
- */
-const _comparePosition = (a, b) => {
-  return a.position - b.position;
-}
-
-@inject(Element, EventAggregator)
 export class SimpleList {
   @bindable listData = [];
 
   simpleListRef;
-  newListData = null;
 
-  constructor(element, eventAggregator) {
+  constructor(element) {
     window.simpleList = this
     this.element = element;
-    this.eventAggregator = eventAggregator;
-    this.db = db;
-
-    this.subscriptions = [];
-
-    this.ctrl = this.sortOrder(this.db.shortcuts);
-    this.commandName = this.sortOrder(this.db.commands);
-
-    this.shortcutStore = {
-      ctrl: this.ctrl,
-      commandName: this.commandName
-    }
+    this.direction = '';
   }
 
   /**
-   * Sort object according to its order propety.
-   * // TODO use value converter
-   * @param {Object} obj 
+   * Iterate through simpleListRef and 
+   * @returns relevant data (htmlElement, position and innerText)
    */
-  sortOrder(arr) {
-    // return (arr);
-    return arr.sort(_comparePosition);
-  }
-
   getDragDropChanges() {
     let newListData = new Array; // Need to declare new array here?
     let children = this.simpleListRef.children
@@ -65,5 +34,13 @@ export class SimpleList {
     }
   }
 
+  sortDescending() {
+    this.direction = 'descending';
+  }
+
+  sortAscending() {
+    this.direction = 'ascending';
+  }
 }
+
 
