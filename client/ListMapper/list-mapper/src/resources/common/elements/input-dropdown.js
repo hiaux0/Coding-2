9860088fc43e0b42771cbe8ec79f8e756fa88182
hdn <hieudn5293@yahoo.de>
2018-08-19 18:12:18 +0200

    Resolve bug: command palett first highlight not shown

--- client/ListMapper/list-mapper/src/resources/common/elements/input-dropdown.js
+++ client/ListMapper/list-mapper/src/resources/common/elements/input-dropdown.js
@@ -1,4 +1,4 @@
-import {bindable} from 'aurelia-framework';
+import {bindable, observable} from 'aurelia-framework';
 import './input-dropdown.less';
 
 export class InputDropdown {
@@ -18,12 +18,20 @@ export class InputDropdown {
    * @param {Boolean}
    */
   @bindable showCommandPalett;
-  /**
-   * @param {String}
-   */
+
   constructor() {
     this.value = window.DEBUG_MODE.commandPalett ? 'Change' : '';
     this.suggestedList = null;
   }
 
+  // attached() {
+  //   console.log(this.commandListContainerRef)
+  //   let children = this.commandListContainerRef.children;
+  //   console.log('​InputDropdown -> attached -> children', children);
+  //   console.log('​InputDropdown -> attached -> children.length', children.length);
+  //   console.log('​InputDropdown -> attached -> children[0]', children[0]);
+  //   if (children) {
+  //     // children[0].classList.add('active');
+  //   }
+  // }
 }
