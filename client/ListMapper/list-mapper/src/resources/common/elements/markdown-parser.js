ad8561513b52ba441890eee9358b94182a7c0b91
hdn <hieudn5293@yahoo.de>
2018-08-25 12:37:42 +0200

    * Markdown parser with enhanced list parsing ** List tags are draggable

--- client/ListMapper/list-mapper/src/resources/common/elements/markdown-parser.js
+++ client/ListMapper/list-mapper/src/resources/common/elements/markdown-parser.js
@@ -1,13 +1,25 @@
-import {inject} from 'aurelia-framework';
+import {bindable, inject, observable} from 'aurelia-framework';
 import {CommandCentral} from '../../common/command-central'
 import {renderMarkdown} from './markdown-parser-custom';
+import {debounce} from 'lodash-decorators';
 
 import './markdown-parser.less';
 
 @inject(CommandCentral)
 export class MarkdownParser {
+  @bindable activateSortable = false;
+
+  @observable inputValue = '- asdasd\n- asdad\n- 23isdjasd\n';
+
+  @debounce(250)
+  inputValueChanged() {
+    this.activateSortable = false;
+    setTimeout(() => {
+      this.convertToHtml();
+    }, 0)
+  }
+
   autoFocus = true;
-  inputValue = 'H~2~0';
 
   constructor(commandCentral) {
     this.commandCentral = commandCentral;
@@ -21,8 +33,11 @@ export class MarkdownParser {
     this.convertToHtml()
   }
 
+  // @debounce(1000)
   convertToHtml = () => {
-    this.result = renderMarkdown(this.inputValue) /*?*/
+    this.result = renderMarkdown(this.inputValue)
+    // console.log('​MarkdownParser -> convertToHtml -> this.result', this.result);
+    this.activateSortable = true;
   }
 
 
