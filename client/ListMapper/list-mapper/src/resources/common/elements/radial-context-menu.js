45a43148ec89f5da783f75f896af30a542938d3d
hdn <hieudn5293@yahoo.de>
2018-08-29 22:07:59 +0200

    Close on outside click

--- client/ListMapper/list-mapper/src/resources/common/elements/radial-context-menu.js
+++ client/ListMapper/list-mapper/src/resources/common/elements/radial-context-menu.js
@@ -1,12 +1,26 @@
-import {bindable} from 'aurelia-framework';
-import './hio-context-menu.less';
+import {bindable, inject} from 'aurelia-framework';
+import close from '../html-helpers/close-on-outside-click';
+import './radial-context-menu.less';
 
-export class HioContextMenu {
+@inject(Element)
+export class RadialContextMenu {
   @bindable value;
   @bindable mouseX;
   @bindable mouseY;
 
+  constructor(element) {
+    this.element = element;
+
+    this.circleRef = null;
+    this.onOutsideClick = null;
+  }
+
   attached() {
+    this.onOutsideClick = close(this.circleRef, () => {
+      console.log('Close now');
+    })
+    window.addEventListener('click', this.onOutsideClick)
+    
     // Demo by http://creative-punch.net
 
     var items = document.querySelectorAll('.circle div');
@@ -20,11 +34,8 @@ export class HioContextMenu {
     setTimeout(() => {
       document.querySelector('.circle').classList.toggle('open');
     }, 0)
-
-    // document.querySelector('.menu-button').onclick = function (e) {
-    //   e.preventDefault(); 
-    //   document.querySelector('.circle').classList.toggle('open');
-    // }
   }
+
+
 }
 
