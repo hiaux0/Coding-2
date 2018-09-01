49755041faaba5445f92d0f8d2e71556daa1438f
hdn <hieudn5293@yahoo.de>
2018-09-01 16:48:13 +0200

    Segemented Button

---
+++ client/ListMapper/list-mapper/src/resources/common/elements/segmented-button.js
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+import './segmented-button.less';
+
+export class SegmentedButton {
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+    //dasd
+  }
+}
