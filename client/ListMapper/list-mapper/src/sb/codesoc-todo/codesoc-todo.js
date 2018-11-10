fdd4fd987f2b6811fbe660881c3bb4bd2bac6b74
hdn <hieudn5293@yahoo.de>
2018-11-10 20:33:58 +0100

    (new) codesoc-todo list coding project just a simple todo list, that will teach concepts of html,css,js

--- client/ListMapper/list-mapper/src/sb/codesoc-todo/codesoc-todo.js
+++ client/ListMapper/list-mapper/src/sb/codesoc-todo/codesoc-todo.js
@@ -2,9 +2,43 @@ import {bindable} from 'aurelia-framework';
 import './codesoc-todo.less'
 
 export class CodesocTodo {
-  @bindable value = 'connected';
 
-  valueChanged(newValue, oldValue) {
+  attached() {
+    let todoList = document.querySelector('.todo-list')
+    let doneList = document.querySelector('.done-list')
+    let todoInput = document.querySelector('.todo-input')
+
+
+    document.addEventListener('keypress', function(ev) {
+      if (ev.which === 116) {
+        ev.preventDefault()
+        todoInput.focus()
+      }
+    })
+
+
+    todoList.addEventListener('click', function(ev) {
+      if (ev.target.tagName !== 'LI') {
+        return;
+      }
+
+      let listItem = ev.target;
+      listItem.classList.toggle('checked');
+      doneList.appendChild(listItem);
+    });
+
+    todoInput.addEventListener('keypress', function(ev) {
+      if (ev.which === 13) {
+        let newTodo = todoInput.value;
+        let newTodoList = document.createElement('li')
+        newTodoList.innerText = newTodo;
+        todoList.appendChild(newTodoList)
+
+        todoInput.value = ''
+      }
+    });
+
+
 
   }
 }
