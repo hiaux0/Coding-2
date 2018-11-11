0c75a2fe0412bff90278f44cff6f2910e2cafc68
hdn <hieudn5293@yahoo.de>
2018-11-11 16:29:33 +0100

    (showcase) of zooming custom attribute

---
+++ client/ListMapper/list-mapper/src/sb/showcase-zooming/showcase-zooming.js
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+import './showcase-zooming.less'
+
+export class ShowcaseZooming {
+  @bindable value = 'ShowcaseZooming';
+
+  valueChanged(newValue, oldValue) {
+
+  }
+}
