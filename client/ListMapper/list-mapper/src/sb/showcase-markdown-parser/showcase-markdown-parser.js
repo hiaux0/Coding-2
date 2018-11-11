edc2a1db018727f8364b07dbe2176c941d9d3d19
hdn <hieudn5293@yahoo.de>
2018-11-11 17:09:40 +0100

    (con) showcase-markdown-parser

---
+++ client/ListMapper/list-mapper/src/sb/showcase-markdown-parser/showcase-markdown-parser.js
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+import './showcase-markdown-parser.less'
+
+export class ShowcaseMarkdownParser {
+  @bindable value = 'ShowcaseMarkdownParser';
+
+  valueChanged(newValue, oldValue) {
+
+  }
+}
