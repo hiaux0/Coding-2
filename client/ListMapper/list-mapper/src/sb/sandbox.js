60e5b86d9f36acbe182d5e4ef456024f92a69483
hdn <hieudn5293@yahoo.de>
2018-11-11 16:29:33 +0100

    (showcase) of zooming custom attribute

--- client/ListMapper/list-mapper/src/sb/sandbox.js
+++ client/ListMapper/list-mapper/src/sb/sandbox.js
@@ -2,7 +2,6 @@ import './sandbox.less'
 
 export class Sandbox {
   activate(params) {
-    console.log('TCL: Sandbox -> activate -> params', params);
     if (params.viewModel) this.viewModel = `./${params.viewModel}/${params.viewModel}`;
   }
 }
