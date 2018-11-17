89f3658c7d57480d14a2c85921d6f6d42b0c59cb
hdn <hieudn5293@yahoo.de>
2018-11-18 00:21:31 +0100

    (add) demo-mask: ability to toggle code behind what you see

--- client/ListMapper/list-mapper/src/sb/demo-mask/demo-mask.js
+++ client/ListMapper/list-mapper/src/sb/demo-mask/demo-mask.js
@@ -1,4 +1,5 @@
 import {bindable} from 'aurelia-framework';
+import { code } from '../../resources/common/styles/icons';
 import './demo-mask.less'
 
 export class DemoMask {
@@ -11,4 +12,12 @@ export class DemoMask {
    * @type {String} codeConent of what you want to display
    */
   @bindable codeContent = 'Your code goes here.'
+
+  codeIcon = code;
+  showCode = false;
+  showMoreOptionsMenu = false;
+
+  toggleCodeMode() {
+    this.showCode = !this.showCode;
+  }
 }
