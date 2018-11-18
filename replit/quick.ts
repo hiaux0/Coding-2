e3b69ddfe35089e39f523fa7c548a914c7bc91ba
hdn <hieudn5293@yahoo.de>
2018-11-18 18:25:55 +0100

    (quick) trim code content value converter implentation scratchpad

--- replit/quick.ts
+++ replit/quick.ts
@@ -1,12 +1,28 @@
+let string: string = `
+        <span id='hello okay'
+          class="hio-border zoom-ex-1"
+          enable-zooming="context: .zoom-ex-1"
+        >Zoom me</span>
+`
 
-function fizzBuzz(num: Number): string {
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzBuzz'
-  if (num % 5 === 0) return 'buzz'
-  if (num % 3 === 0) return 'fizz'
-  return num.toString();
-}
+let splittedAfterNewLine: string = string.split('\n');
+let noEmptyLines: string = splittedAfterNewLine.filter(line => line !== '');
+noEmptyLines[0] /*?*/
 
-for (let i: Number = 0; i < 31; i++) {
-  let res = fizzBuzz(i)
-  console.log(res)
-}
+
+let numOfTopLevelWhiteSpace: string = noEmptyLines[0].match(/(\s)/) /*?*/
+console.log("​numOfTopLevelWhiteSpace", numOfTopLevelWhiteSpace)
+
+let numOfTopLevelWhiteSpace: string = noEmptyLines[0].split(/\s/) /*?*/
+
+let res = /\s/.exec(noEmptyLines[0]) /*?*/
+
+let trimmedLines: string[] = noEmptyLines.map(line => {
+  return line.substring(numOfTopLevelWhiteSpace);
+});
+
+// trimmedLines
+let joined: string = trimmedLines
+                      .join()
+                      .replace(/,/g, '\n')
+
