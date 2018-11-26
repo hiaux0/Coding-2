20e6540d25a4182cc7269491ee55e21c0e51f0d0
hdn <hieudn5293@yahoo.de>
2018-11-26 22:59:24 +0100

    (quick) noChildren

--- replit/quick.ts
+++ replit/quick.ts
@@ -1,28 +1,31 @@
-let string: string = `
-        <span id='hello okay'
-          class="hio-border zoom-ex-1"
-          enable-zooming="context: .zoom-ex-1"
-        >Zoom me</span>
-`
 
-let splittedAfterNewLine: string = string.split('\n');
-let noEmptyLines: string = splittedAfterNewLine.filter(line => line !== '');
-noEmptyLines[0] /*?*/
+const foldersMock = [
+  { title: 'f-root-1', id: '1', parentId: null },
+  { title: 'f-nested-1-1', id: '2', parentId: '1' },
+  { title: 'f-nested-1-1-1', id: '3', parentId: '2' },
+  { title: 'f-nested-1-1-2', id: '4', parentId: '2' },
+  { title: 'f-nested-1-1', id: '5', parentId: '1' },
+  { title: 'f-root-2', id: '6', parentId: null }
+];
+
+const projectsMock = [
+  { title: 'p-root-1', id: '7', parentId: null },
+  { title: 'p-nested-1-1', id: '8', parentId: '1' },
+  { title: 'p-nested-1-1-1', id: '9', parentId: '2' },
+  { title: 'p-nested-1-1-2', id: '10', parentId: '2' },
+  { title: 'p-nested-1-2', id: '11', parentId: '1' },
+  { title: 'p-root-2', id: '12', parentId: '6' },
+  { title: 'p-root-3', id: '13', parentId: null }
+];
 
 
-let numOfTopLevelWhiteSpace: string = noEmptyLines[0].match(/(\s)/) /*?*/
-console.log("​numOfTopLevelWhiteSpace", numOfTopLevelWhiteSpace)
+const noChildren = (filterId) => {
+  let isParentOfParent = foldersMock.some(folder => folder.parentId === filterId)
+  let isParentOfProjects = projectsMock.some(project => project.parentId === filterId)
+  return !isParentOfParent && !isParentOfProjects;
+}
 
-let numOfTopLevelWhiteSpace: string = noEmptyLines[0].split(/\s/) /*?*/
-
-let res = /\s/.exec(noEmptyLines[0]) /*?*/
-
-let trimmedLines: string[] = noEmptyLines.map(line => {
-  return line.substring(numOfTopLevelWhiteSpace);
-});
-
-// trimmedLines
-let joined: string = trimmedLines
-                      .join()
-                      .replace(/,/g, '\n')
-
+noChildren('5') /*?*/
+noChildren('6') /*?*/
+noChildren('2') /*?*/
+noChildren('1') /*?*/
