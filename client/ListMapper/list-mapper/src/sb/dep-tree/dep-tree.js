fffa604023e4fc9ee1a040b4363bf6b477319266
hdn <hieudn5293@yahoo.de>
2018-12-08 23:02:05 +0100

    (styles) dep-tree: styles for file cards

--- client/ListMapper/list-mapper/src/sb/dep-tree/dep-tree.js
+++ client/ListMapper/list-mapper/src/sb/dep-tree/dep-tree.js
@@ -1,17 +1,20 @@
 import {bindable} from 'aurelia-framework';
 import './dep-tree.less'
 import { HttpClient } from 'aurelia-fetch-client';
+import hotkeys from 'hotkeys-js';
+
 
 export class DepTree {
   @bindable value = 'DepTree';
 
-  valueChanged(newValue, oldValue) {
-
-  }
+  depTree;
 
   bind() {
     this.getDepTree();
+    hotkeys("c", () => console.clear()); // eslint-disable-line no-console
+    hotkeys("h", () => console.log(this));  // eslint-disable-line no-console
   }
+
 
   getDepTree() {
     let client = new HttpClient();
@@ -19,6 +22,7 @@ export class DepTree {
     client.fetch('http://localhost:3030/dep-tree')
       .then(response => response.json())
       .then(data => {
+        this.depTree = new Map(data);
         console.log(data);
       });
   }
