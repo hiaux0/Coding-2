2016ee9938ac265e9460bac9db9d719a99b4d426
hdn <hieudn5293@yahoo.de>
2018-12-15 11:56:02 +0100

    (quick) neo4j string builder

--- replit/quick.js
+++ replit/quick.js
@@ -1,19 +1,30 @@
-let test = new Map()
+// createNode
+// node name
+// properties
 
-test.set(1, 's')
+function createNode(nodeName, properties) {
+  return `CREATE (a:${nodeName} ${JSON.stringify(properties)})`;
+}
 
-test /*?*/
+createNode('what', {name: 'hello', age: 15, saying: 'hello'}) /*?*/
 
-let newMap = new Map(test);
-newMap.set(test) /
+let string = 'CREATE (a:what {"name":"hello","age":15,"saying":"hello"})​​​​​';
+string.replace(/{"/g, '{')
+      .replace(/,"/g,",")
+      .replace(/":/g,":") /*?*/
+// createPropertyString
 
+function neoParamsBuilder(params) {
+  let stringParams = JSON.stringify(params);
+  const removeQuotesFromKey = (match) => match.replace(/"/g, "");
+  // match words between `{...:` and `,...:`.
+  return stringParams.replace(/({|,)"\w+"/g, removeQuotesFromKey);
+}
 
+neoParamsBuilder({ name: 'hello', age: 15, saying: 'hello' }) /*?*/
 
+// properties will need a query
 
-
-
-
-
-
-
-
+// { name: 'hiau', age: 15 }
+// create(p: person { name: 'hiau', age: 15 }) return p
+// create(p: person { name: $name, age: $age }) return p
