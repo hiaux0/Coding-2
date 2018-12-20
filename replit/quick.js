5cfd8f3a637bd877f47530bd952e94e4e66c4a26
hdn <hieudn5293@yahoo.de>
2018-12-20 22:47:18 +0100

    (replit) promise.all and maps

--- replit/quick.js
+++ replit/quick.js
@@ -1,45 +1,17 @@
-// createNode
-// node name
-// properties
+let mapper = new Map();
+mapper.size /*?*/
 
-function createNode(nodeName, properties) {
-  return `CREATE (a:${nodeName} ${JSON.stringify(properties)})`;
-}
 
-createNode('what', {name: 'hello', age: 15, saying: 'hello'}) /*?*/
 
-let string = 'CREATE (a:what {"name":"hello","age":15,"saying":"hello"})​​​​​';
-string.replace(/{"/g, '{')
-      .replace(/,"/g,",")
-      .replace(/":/g,":") /*?*/
-// createPropertyString
+mapper.set('hi', 'hi')
+mapper.set('what', 'hi')
+mapper.set('eshtn', 'hi')
+mapper.set('ehst', 'hi')
 
-function neoParamsBuilder(params) {
-  let stringParams = JSON.stringify(params);
-	console.log("​stringParams", stringParams)
-  const removeQuotesFromKey = (match) => {
-    console.log(match)
-    return match.replace(/"/g, "");
-  }
-  // match words between `{...:` and `,...:`.
-  return stringParams.replace(/({|,)"\w+":/g, removeQuotesFromKey);
-}
+// mapper.forEach((value, key) => {
+//   key
+//   value
+// })
 
-neoParamsBuilder({ name: 'hello', age: ['what', 'hello'] }) /*?*/
-
-// properties will need a query
-
-// { name: 'hiau', age: 15 }
-// create(p: person { name: 'hiau', age: 15 }) return p
-// create(p: person { name: $name, age: $age }) return p
-
-function neoRelationshipBuilder({ nodeVars, relationshipName, params }) {
-  params = neoParamsBuilder(params);
-  let firstNode = nodeVars[0];
-  let secondNode = nodeVars[1];
-  return `(${firstNode}) - [:${relationshipName} ${params}] - (${secondNode});`;
-}
-
-neoRelationshipBuilder({ nodeVars: ['a', 'b'], relationshipName: 'KNOWS', params: {since: 2011} }); /*?*/
-
-""+-13
+let vals = mapper.values();
+Array.from(vals).some(val => val === 'hi') /*?*/
