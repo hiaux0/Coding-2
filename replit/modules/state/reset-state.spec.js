39a477775ba75daba22465a4e5596ce9cf359cb9
hdn <hieudn5293@yahoo.de>
2018-12-22 12:03:34 +0100

    (repl) reset-state

---
+++ replit/modules/state/reset-state.spec.js
@@ -0,0 +1,32 @@
+const {resetState} = require('./reset-state');
+
+fdescribe('resetState', () => {
+  let initialState = { a: null , b: null , c: null , d: 'no touch' };
+  let editedState = { a: 2 , b: 3 , c: 4 , d: 'dirty' };
+
+  let reset = resetState({
+    initialState,
+    editedState,
+    keys: ['a', 'b', 'c']
+  });
+
+  it('Should reset state based on keys', () => {
+    expect(JSON.stringify(reset.a)).toBe(JSON.stringify(initialState.a));
+    expect(JSON.stringify(reset.b)).toBe(JSON.stringify(initialState.b));
+    expect(JSON.stringify(reset.c)).toBe(JSON.stringify(initialState.c));
+  });
+
+  it('Should not touch other keys', () => {
+    expect(JSON.stringify(reset.d)).toBe(JSON.stringify(editedState.d));
+   });
+
+  it('Should throw on keys which are not present in intial state', () => {
+    expect(function() {
+      resetState({
+        initialState,
+        editedState,
+        keys: ['z']
+      })
+    }).toThrow('`editedState` does not have a property "z"')
+  });
+});
