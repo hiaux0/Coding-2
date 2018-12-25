9636d39258105c48cd650f219b7fd64983742698
hdn <hieudn5293@yahoo.de>
2018-12-25 15:34:52 +0100

    (config, c) wallay.config for using in the client

--- client/ListMapper/list-mapper/wallaby.config.js
+++ client/ListMapper/list-mapper/wallaby.config.js
@@ -1,33 +1,27 @@
-var wallabyWebpack = require('wallaby-webpack');
+// var wallabyWebpack = require('wallaby-webpack');
 
-var webpackConfig = require('./webpack.config');
-var wallabyPostprocessor = wallabyWebpack(webpackConfig);
+// var webpackConfig = require('./webpack.config');
+// var wallabyPostprocessor = wallabyWebpack(webpackConfig);
 
 module.exports = function (wallaby) {
   return {
     files: [
       // { pattern: '**/*.js', load: true },
-      { pattern: 'src/**/*.js', load: false },
-      { pattern: 'src/**/*.spec.js', ignore: true },
+      { pattern: 'src/**/modules/*.js', load: false },
+      { pattern: 'src/**/modules/**/*.spec.js', ignore: true },
     ],
 
     tests: [
-      { pattern: 'src/**/*.spec.js', load: true },
-      { pattern: 'node_modules/', ignore: true }
+      { pattern: 'src/**/modules/**/*.spec.js', load: true },
     ],
     env: {
-      type: 'browser'
+      type: 'node'
     },
-    postprocessor: wallabyPostprocessor,
+    // postprocessor: wallabyPostprocessor,
 
     debug: true,
     testFramework: "jasmine",
     compilers: {
-      // '**/*.js': wallaby.compilers.babel({
-      //   // babelrc: true,
-      //   presets: ['es2015-loose', 'stage-1'],
-      //   plugins: ['transform-decorators-legacy']
-      // })
       '**/*.js': wallaby.compilers.babel({
         babelrc: true,
         presets: ['es2015', 'stage-1'],
