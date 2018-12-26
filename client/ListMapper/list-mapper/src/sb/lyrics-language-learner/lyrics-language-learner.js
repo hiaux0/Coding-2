462c31178bc7faffb8067c24f89f0dd720f8f804
hdn <hieudn5293@yahoo.de>
2018-12-26 13:53:58 +0100

    (add) lyrics-language-learner: save to local storage

--- client/ListMapper/list-mapper/src/sb/lyrics-language-learner/lyrics-language-learner.js
+++ client/ListMapper/list-mapper/src/sb/lyrics-language-learner/lyrics-language-learner.js
@@ -4,6 +4,7 @@ import './lyrics-language-learner.less'
 import { tokenizeLyrics } from './modules/tokenize-lyrics';
 import {translate} from './lyrics-language-learner.gateway';
 import { testLyrics } from './modules/test-data';
+import hotkeys from 'hotkeys-js';
 
 export class LyricsLanguageLearner {
   @bindable lyrics = testLyrics;
@@ -19,8 +20,15 @@ export class LyricsLanguageLearner {
   sidebarLyricWord = 'change';
 
   bind() {
+    window.localStorage.clear();
     this.lyricsMap = tokenizeLyrics(this.lyrics);
     console.log(this.lyricsMap);
+  }
+
+  attached() {
+    hotkeys("c", () => console.clear()); // eslint-disable-line no-console
+    hotkeys("h", () => console.log(this));  // eslint-disable-line no-console
+    hotkeys("d", () => window.localStorage.clear());  // eslint-disable-line no-console
   }
 
   /**
@@ -29,9 +37,28 @@ export class LyricsLanguageLearner {
   passDataToSidebar = (event) => {
     const target = event.target;
     this.sidebarLyricWord = target.innerText;
-    translate(this.sidebarLyricWord).then(response => {
+
+    if (document.getSelection().toString()) {
+      this.sidebarLyricWord = document.getSelection().toString();
+    }
+
+    if (window.localStorage.getItem(this.sidebarLyricWord)) {
+      console.log('Got from local storage');
+      const rawFromStorage = window.localStorage.getItem(this.sidebarLyricWord);
+      const storageData = JSON.parse(rawFromStorage);
+      this.sidebarLyricTranslation = storageData;
+    } else {
+      console.log('New request');
+      // translate(this.sidebarLyricWord).then(response => {
+      //   this.sidebarLyricTranslation = response;
+      // });
+
+      const response = [
+        { translation: this.sidebarLyricWord + ' test' }
+      ]
+      window.localStorage.setItem(this.sidebarLyricWord, JSON.stringify(response));
       this.sidebarLyricTranslation = response;
-    });
+    }
   }
 
 }
