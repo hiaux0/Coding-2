40f6dd19b80073f447e4ebe64de54e6b7859bd8c
hdn <hieudn5293@yahoo.de>
2018-12-26 03:16:12 +0100

    (connect) lyrics FE to BE - split up test string

---
+++ client/ListMapper/list-mapper/src/sb/lyrics-language-learner/lyrics-language-learner.gateway.js
@@ -0,0 +1,47 @@
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { Container } from 'aurelia-dependency-injection';
+
+export const translate = (stringArr) => {
+  const client = Container.instance.get(HttpClient);
+
+  stringArr = Array.isArray(stringArr) ? stringArr : [stringArr];
+  stringArr.join(',');
+
+  return client.fetch(`http://localhost:3030/translate/${stringArr}`)
+    .then(response => response.json())
+    .then(data => {
+      return data.translations;
+    });
+
+}
+
+// {
+//   "translations": [
+//     {
+//       "translation": "A handsome one."
+//     }
+//   ],
+//   "word_count": 1,
+//   "character_count": 3
+// }
+
+/**
+ * text string[]
+Input text in UTF-8 encoding. Multiple entries will result in multiple translations in the response.
+
+model_id string
+A globally unique string that identifies the underlying model that is used for translation.
+
+source string
+Translation source language code.
+
+target string
+Translation target language code.
+ */
+
+// curl -X POST -u "apikey:{apikey}" --header "Content-Type: application/json" --data "{\"text\": [\"Hello, world!\", \"How are you?\"], \"model_id\":\"en-es\"}" "{url}/v3/translate?version=2018-05-01"
+
+/**
+curl --user apikey:S7DU1G82CyZy2ng6ZKIGWtb3Gm1ZgY0elAhelOG5mUmV --request POST --header "Content-Type: application/json" --data '
+{"text":["Hello"],"model_id":"en-es"}' "https://gateway-fra.watsonplatform.net/language-translator/api/v3/translate?version=2018-05-01"
+ */
