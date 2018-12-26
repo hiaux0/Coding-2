a4f0f15089bf098d4ec1293de69353792b49f220
hdn <hieudn5293@yahoo.de>
2018-12-26 03:13:18 +0100

    (change) close-on-outside-click: first update after initial implentation - minor clean ups - small docs

--- client/ListMapper/list-mapper/src/resources/common/html-helpers/close-on-outside-click.js
+++ client/ListMapper/list-mapper/src/resources/common/html-helpers/close-on-outside-click.js
@@ -1,26 +1,23 @@
 /**
+ * const context = <htmlElement>;
+ * const closeCallback = close(context, (ev) => {});
+ * document.addEventListener('click', closeCallback);
  * @param {HTMLElement} context - Close on outside click of context.
  * @param {Function} cb - Callback to perform on outside click.
- * @return {Function} 
+ * @return {Function}
  *    @param {Object} ev - Html event object.
  */
 export default close = (context, cb) => (ev) => {
   let target = ev.target;
-  if (target === context) {
-    return;
-  }
+  if (target === context) return;
 
   let parent = target.parentElement;
   do {
     parent = parent.parentElement
-    if (parent === context) {
-      return;
-    }
-    else if (parent.id === 'hio-body') {
-      break;
-    }
+    if (parent === context) return;
+    else if (parent.id === 'hio-body') break;
 
   } while (parent !== context);
 
-  if (typeof cb === 'function') cb();
+  if (typeof cb === 'function') cb(ev);
 }
