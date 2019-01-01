78f2ec022dbe46213ec9ea7dc5082e07c11ef51a
hdn <hieudn5293@yahoo.de>
2019-01-01 18:11:25 +0100

    (add)(change) songs-controller.js - file const tableName - listSongs - convert JSON string to JSON before sending to client - save lyrics text to tokenized array

--- server/my-sql/mysql-controllers/songs-controller.js
+++ server/my-sql/mysql-controllers/songs-controller.js
@@ -1,23 +1,39 @@
 const apiInit = require('../api/api-init');
+const { tokenizeLyrics } = require('../lyrics/tokenize-lyrics');
 
-const songsTableName = 'songs';
+const tableName = 'songs';
+
+exports.listSongs = ((req, res) => {
+  apiInit.listEntries({tableName})
+    .then(lyricEntries => {
+      lyricEntries.forEach(entry => {
+        entry.lyrics = JSON.parse(entry.lyrics);
+      })
+      // todo auto formatter to pares json (also use it in `getEntry`)
+      res.json(lyricEntries);
+    })
+})
 
 exports.getSong = ((req, res) => {
-  const { params } = req;
-  console.log("​params", params)
-  const { songId } = params;
+  const { songId } = req.params;
 
-  apiInit.getEntry({ id: songId, tableName: songsTableName })
-    .then(data => {
-      res.json(data);
+  apiInit.getEntry({ id: songId, tableName })
+    .then(lyricsEntry => {
+      // is saved as string in database, thus need to convert.
+      lyricsEntry[0].lyrics = JSON.parse(lyricsEntry[0].lyrics);
+      res.json(lyricsEntry);
     })
 })
 
 exports.postSong = ((req, res) => {
-    const {body} = req;
-    console.log("body", body)
+  const {lyrics} = req.body;
+  let lyricsMap = tokenizeLyrics(lyrics);
+  let lyricsData = Array.from(lyricsMap);
+  lyricsData = JSON.stringify(lyricsData);
 
-    apiInit.insert({tableName: songsTableName, data: body})
+  apiInit.insert({ tableName, data: {
+    lyrics: lyricsData
+  }})
     .then(data => {
       res.json(data);
     })
